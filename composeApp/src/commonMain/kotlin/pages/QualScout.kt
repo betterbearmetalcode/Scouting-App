467414438f67addcb271fd84aa8ee726763ccd2d
Asher <asher@raisingheller.com>
2023-12-18 11:45:11 -0800

    Cleaning up

---
+++ composeApp/src/commonMain/kotlin/pages/QualScout.kt
@@ -0,0 +1,9 @@
+package pages
+
+import com.bumble.appyx.navigation.modality.BuildContext
+import com.bumble.appyx.navigation.node.Node
+
+class QualScout(
+    buildContext: BuildContext
+) : Node( buildContext = buildContext) {
+}
---
+++ composeApp/src/commonMain/kotlin/pages/QuanScout.kt
@@ -0,0 +1,9 @@
+package pages
+
+import com.bumble.appyx.navigation.modality.BuildContext
+import com.bumble.appyx.navigation.node.Node
+
+class QuanScout(
+    buildContext: BuildContext
+) : Node( buildContext = buildContext) {
+}
