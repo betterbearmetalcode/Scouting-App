5db4a08f355999a3feb043cef87c2b29c7c6501e
milkchunks <bendod2020@gmail.com>
2024-03-12 16:56:53 -0700

    Made Possibly Working Image Export for Mech scouting

---
+++ composeApp/src/androidMain/kotlin/composables/Download.kt
@@ -0,0 +1,20 @@
+package composables
+
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asAndroidBitmap
+import java.io.ByteArrayOutputStream
+import java.io.File
+
+fun pitsDownload(
+    context: Context,
+    photoArray: ArrayList<ImageBitmap>,
+    teamNumber: String,
+) {
+    val file = File(context.filesDir, "Primary$teamNumber.png")
+    val stream = ByteArrayOutputStream()
+    photoArray[0].asAndroidBitmap().compress(Bitmap.CompressFormat.PNG, 100, stream)
+    file.writeBytes(stream.toByteArray())
+    file.createNewFile()
+}
