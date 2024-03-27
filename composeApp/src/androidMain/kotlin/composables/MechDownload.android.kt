050191d262ad516f438eeabce6ddb1d971cc4369
milkchunks <bendod2020@gmail.com>
2024-03-26 17:03:20 -0700

    pits download still doesn't work

--- composeApp/src/androidMain/kotlin/composables/MechDownload.android.kt
+++ composeApp/src/androidMain/kotlin/composables/MechDownload.android.kt
@@ -2,17 +2,25 @@ package composables
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.graphics.ImageDecoder.decodeBitmap
 import android.net.Uri
+import android.os.Build
 import android.provider.MediaStore
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.graphics.decodeBitmap
+import androidx.core.net.toUri
+import com.bumble.appyx.interactions.core.asElement
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 
 
+@RequiresApi(Build.VERSION_CODES.P)
 @Composable
 fun download(
     context: Context,
@@ -20,21 +28,15 @@ fun download(
     teamNumber: String,
     photoAmount: Int
 ) {
-    //var file = File(context.cacheDir, "photo_0.jpg")
-    val file = File(context.cacheDir,"primary$teamNumber")
+    var file = File(context.cacheDir,"primary$teamNumber")
     val directory = context.filesDir
+    directory.isDirectory
     directory.mkdirs()
     file.delete()
     file.createNewFile()
-    var bos = ByteArrayOutputStream();
-    var bitmapdata = photoArray[0]
-    //var byte = ByteArray(photoArray[0].size)
-    // Assuming you're inside a Composable function
-    val context = LocalContext.current
-    val contentResolver = context.contentResolver
-    val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, photoArray[0])
-    // Your image data
-    bitmap.compress(Bitmap.CompressFormat.PNG,0,bos)
-//    val fos: FileOutputStream = FileOutputStream(file)
-//    fos.write(byteArray)
+    val os = FileOutputStream(file);
+    val contentResolver =  LocalContext.current.contentResolver
+    val bitmap = decodeBitmap(ImageDecoder.createSource(contentResolver, photoArray[0]))
+    bitmap.compress(Bitmap.CompressFormat.PNG,0,os)
+    os.flush()
 }
